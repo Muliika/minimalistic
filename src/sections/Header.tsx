231f85fa47124b5c7b84c87236cc074bd87a029d
Muliika <samuelmuliika@gmail.com>
2025-08-02 20:40:23 +0300

    start

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,10 @@
 export const Header = () => {
-  return <div>Header Section</div>;
+  return <div>
+    <nav>
+      <a href="#" className="">Home</a>
+      <a href="#" className="">Projects</a>
+      <a href="#" className="">About</a>
+      <a href="#" className="">Contact</a>
+    </nav>
+  </div>;
 };
